79478b35aeb5eec32701f778e0505b67dceb53e9
ben <ben.raine@engineering.digital.dwp.gov.uk>
2023-08-04 14:06:30 +0100

    set up
    GitOrigin-RevId: dfce29a8d72147cc819b873eb376f68a7392d422

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -19,56 +19,9 @@ Example usage:
 
 module.exports = {
   'email-address': 'sarah.phillips@example.com',
-  'mobile-number': '07700 900457',
-  'confirm-details-change': 'yes',
-  'left-over-income':'100.00',
-
-  'income-monthly':'0.00',
-  'benefits':'0.00',
-  'pension':'0.00',
-  'other-income':'0.00',
-
-  'paymentOptions' : 'ddFixedChoice',
-'additional-amount' :'0.00',
-
-  'housing':'0.00',
-  'utilities':'0.00',
-  'travel':'0.00',
-  'pension-contributions':'0.00',
-  'groceries':'0.00',
-  'childcare-costs':'0.00',
-  'personal-costs':'0.00',
-  'communications':'0.00',
-  'other-repayments':'0.00',
-
-
-
-
+ 'mobile-number': '07700 900457'
+ 'confirm-details-change': 'yes'
 
   // Insert values here
-
-
-  'income-other':'0',
-  'income-universal-credit':'0',
-  'income-take-home':'0',
-  'income-pension':'0',
-
-  'spending-other':'0',
-  'spending-caring':'0',
-  'spending-groceries':'0',
-  'spending-travel':'0',
-  'spending-utilities':'0',
-  'spending-phone':'0',
-  'spending-housing':'0',
-  'dd-reminder-notification': 'no',
-  'dd-directdebit-day': '02',
-  'dd-payment-amount':'20.00',
-//  'reminder-notification-payment-setup-done-yes-no':'no',
-  'contactByMobile-masked': '********302'
-
-
-
-
-
 
 }
