e1559e019cc205d0b2a53e57392c6989f2053da0
Sarat Kumar <sarat.kumar@engineering.digital.dwp.gov.uk>
2024-02-01 13:41:02 +0000

    JD User research and prototype upgrade
    GitOrigin-RevId: bc398ae5ed301b964c9389ad16ee26144bac8181

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,16 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+
+function showDate() {
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+let month = months[d.getMonth()];
+let date = d.getDate();
+document.getElementById("date").innerHTML = date	+ " " + month + " " + d.getFullYear() ;
+}
+
+
+
 })
